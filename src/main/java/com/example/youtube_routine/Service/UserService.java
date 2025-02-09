2f413fb811d757592a92d4618cc3b988b5da879b
lbk00 <lbk6661@gmail.com>
2025-02-09 18:55:05 +0900

    User 서비스

--- src/main/java/com/example/youtube_routine/Service/UserService.java
+++ src/main/java/com/example/youtube_routine/Service/UserService.java
@@ -5,5 +5,7 @@ import com.example.youtube_routine.Entity.User;
 public interface UserService {
     //디바이스 번호 받고 사용자 객체 생성
     User registerUser(String deviceId);
+    // 디바이스 번호로 사용자 조회
+    User getUserByDeviceId(String deviceId);
 
 }
--- src/main/java/com/example/youtube_routine/Service/UserServiceImpl.java
+++ src/main/java/com/example/youtube_routine/Service/UserServiceImpl.java
@@ -1,11 +1,29 @@
 package com.example.youtube_routine.Service;
 
 import com.example.youtube_routine.Entity.User;
+import com.example.youtube_routine.Repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
 
     @Override
     public User registerUser(String deviceId) {
-        return null;
+        return userRepository.findByDeviceId(deviceId)
+                .orElseGet(() -> userRepository.save(
+                        User.builder()
+                                .deviceId(deviceId)
+                                .build()));
+    }
+
+    @Override
+    public User getUserByDeviceId(String deviceId) {
+        return userRepository.findByDeviceId(deviceId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
     }
 }
