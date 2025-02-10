7e3030678406110444302e801aae221c2b5b2d18
lbk00 <lbk6661@gmail.com>
2025-02-10 14:35:38 +0900

    NotificationController 추가 ( 알림 푸시 기능 )

--- src/main/java/com/example/youtube_routine/Controller/NotificationController.java
+++ src/main/java/com/example/youtube_routine/Controller/NotificationController.java
@@ -1,2 +1,28 @@
-package com.example.youtube_routine.Controller;public class NotificationController {
+package com.example.youtube_routine.Controller;
+
+import com.example.youtube_routine.Service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    // 설정한 루틴의 시간이 되었을 때 알림 푸시
+    @GetMapping("/send/{routineId}")
+    public ResponseEntity<String> sendNotification(@PathVariable Long routineId) {
+        notificationService.sendNotification(routineId);
+        return ResponseEntity.ok("푸시 알림 전송 완료");
+    }
+
+    // 알림 클릭 시 유튜브 링크 반환
+    @GetMapping("/open/{routineId}")
+    public ResponseEntity<String> openRoutineLink(@PathVariable Long routineId) {
+        String youtubeLink = notificationService.getYoutubeLink(routineId);
+        return ResponseEntity.ok(youtubeLink);
+    }
 }
