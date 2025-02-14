37ca74a81d0de126c2011d08bba3c12e70cc91a0
lbk00 <lbk6661@gmail.com>
2025-02-14 16:41:16 +0900

    getDeviceId - 기기 고유번호 가져오는 임시 메서드 추가

--- src/main/java/com/example/youtube_routine/Service/UserService.java
+++ src/main/java/com/example/youtube_routine/Service/UserService.java
@@ -3,9 +3,13 @@ package com.example.youtube_routine.Service;
 import com.example.youtube_routine.Entity.User;
 
 public interface UserService {
+    // 디바이스 번호를 가져오는 메서드
+    String getDeviceId();
     //디바이스 번호 받고 사용자 객체 생성
     User registerUser(String deviceId);
     // 디바이스 번호로 사용자 조회
-    User getUserByDeviceId(String deviceId);
+    User getUser(String deviceId);
+    // 디바이스 번호로 사용자 삭제
+    void deleteUser(String deviceId);
 
 }
--- src/main/java/com/example/youtube_routine/Service/UserServiceImpl.java
+++ src/main/java/com/example/youtube_routine/Service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.youtube_routine.Service;
 
 import com.example.youtube_routine.Entity.User;
+import com.example.youtube_routine.Repository.RoutineRepository;
 import com.example.youtube_routine.Repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,14 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final RoutineRepository routineRepository;
+
+    // 사용자가 어플을 최초 실행시 기기 고유번호릉 가져옴
+    // 기기번호는 클라이언트에서 가져온 후 백엔드에 전달
+    @Override
+    public String getDeviceId() {
+        return "";
+    }
 
     @Override
     public User registerUser(String deviceId) {
@@ -22,8 +31,20 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User getUserByDeviceId(String deviceId) {
+    public User getUser(String deviceId) {
         return userRepository.findByDeviceId(deviceId)
                 .orElseThrow(() -> new EntityNotFoundException("User not found"));
     }
+
+    // 사용자 삭제
+    public void deleteUser(String deviceId) {
+        User user = userRepository.findByDeviceId(deviceId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        // 해당 사용자의 루틴 삭제
+        routineRepository.deleteAllByUser(user);
+        // 사용자 삭제
+        userRepository.delete(user);
+    }
+
 }
