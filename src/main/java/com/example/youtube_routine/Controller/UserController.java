2e3a9d8dd53631293a361c6257d31d15e2dbd3f5
lbk00 <lbk6661@gmail.com>
2025-02-15 15:47:27 +0900

    UserController 수정

--- src/main/java/com/example/youtube_routine/Controller/UserController.java
+++ src/main/java/com/example/youtube_routine/Controller/UserController.java
@@ -1,38 +1,44 @@
 package com.example.youtube_routine.Controller;
 
+import com.example.youtube_routine.Entity.User;
+import com.example.youtube_routine.Service.UserService;
+import com.example.youtube_routine.Service.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/users")
+@RequiredArgsConstructor
 public class UserController {
     // 사용자는 띠로 id/ pw 없이 로그인 가능
     // 사용자의 기기의 고유번호로 사용자 엔티티 생성
 
+    private final UserService userService;
+
     // api
     // 1. 앱을 처음 실행했을때, 고유 디바이스번호로 사용자 객체 생성 post
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String register(@RequestBody String deviceId) {
-        // 디바이스 번호 받고 사용자 객체 생성
-//        User user = userService.registerUser(deviceId);
-        return "test";
+    public ResponseEntity<User> register(@RequestBody String deviceId) {
+//        String findDeviceId = userService.findDeviceId();
+        // 사용자별로 디바이스 id 찾아서 반환
+        // 그 후, deviceId로 사용자 등록
+        User user = userService.registerUser(deviceId);
+        return ResponseEntity.ok(user);
     }
 
-    // 2. 사용자가 새로운 루틴 생성 post
+    // 사용자 조회 api (deviceId로 조회)
+    @GetMapping("/{deviceId}")
+    public ResponseEntity<User> getUserByDeviceId(@PathVariable String deviceId) {
+        User user = userService.getUser(deviceId);
+        return ResponseEntity.ok(user);
+    }
 
-    // 3. 사용자 별 루틴 조회 get
-    // id 또는 deviceId로 조회
-
-    // 4. 사용자가 기존 루틴 수정 put
-
-    // 5. 사용자가 기존 루틴 삭제 delete
-
-    // 6. 사용자가 설정한 루틴의 시간이 되었을때 알림 푸시 + 간단한 메시지도 같이 나오게
-
-    // 7. 사용자가 알림 누르면 바로 유튜브 링크 나오도록
-
-
+    // 사용자 삭제 api (앱 삭제 시 호출)
+    @DeleteMapping("/{deviceId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String deviceId) {
+        userService.deleteUser(deviceId); // 사용자 및 관련 데이터 삭제
+        return ResponseEntity.noContent().build();
+    }
 }
