ab3f51ea7a30fb3595dda14d35dbed850f16caa7
lbk00 <lbk6661@gmail.com>
2025-02-16 13:00:08 +0900

    DTO - deviceId 삭제

--- src/main/java/com/example/youtube_routine/Entity/RoutineRequestDTO.java
+++ src/main/java/com/example/youtube_routine/Entity/RoutineRequestDTO.java
@@ -7,10 +7,10 @@ import java.time.LocalTime;
 // 새로운 루틴을 만들기위해 필요한 DTO
 @Data
 public class RoutineRequestDTO {
-    private String deviceId; // 유지 (핸드폰 고유 식별자)
     private Day day;
     private LocalTime routineTime;
     private String youtubeLink;
     private String content; // 메시지
+    private boolean repeat;
 }
 
