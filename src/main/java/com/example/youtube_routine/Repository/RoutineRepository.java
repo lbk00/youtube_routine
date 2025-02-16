4a19c66c3129528993482cb4e19200ee308c0de6
lbk00 <lbk6661@gmail.com>
2025-02-16 13:01:08 +0900

    findByUserDeviceId - 쿼리 추가

--- src/main/java/com/example/youtube_routine/Repository/RoutineRepository.java
+++ src/main/java/com/example/youtube_routine/Repository/RoutineRepository.java
@@ -3,11 +3,14 @@ package com.example.youtube_routine.Repository;
 import com.example.youtube_routine.Entity.Routine;
 import com.example.youtube_routine.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
-    List<Routine> findByUser(User user);
+    @Query("SELECT r FROM Routine r JOIN r.user u WHERE u.deviceId = :deviceId")
+    List<Routine> findByUserDeviceId(@Param("deviceId") String deviceId); // 루틴에 deviceId 필드 추가 없이 조회
     // 특정 사용자에 대한 모든 루틴 삭제
     void deleteAllByUser(User user);
 
