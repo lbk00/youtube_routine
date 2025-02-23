9d45609ef95a56f881e2e54ad73d46c0ddbe809a
lbk00 <lbk6661@gmail.com>
2025-02-23 12:51:23 +0900

    Notification 패키지 삭제

--- src/main/java/com/example/youtube_routine/Notification/NotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.youtube_routine.Notification;
-
-import com.example.youtube_routine.Routine.Routine;
-
-public interface NotificationService {
-    //fcm토큰으로 사용자의 루틴 목록 조회
-    void sendPushNotificationByFcmToken(String fcmToken);
-}
