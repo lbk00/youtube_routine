783caf5d22b2c33402450f69563698c24ab9c62b
lbk00 <lbk6661@gmail.com>
2025-03-21 20:18:29 +0900

    스케줄러 사용하기위한 @EnableScheduling 추가

--- src/main/java/com/example/youtube_routine/YoutubeRoutineApplication.java
+++ src/main/java/com/example/youtube_routine/YoutubeRoutineApplication.java
@@ -2,8 +2,10 @@ package com.example.youtube_routine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class YoutubeRoutineApplication {
 
     public static void main(String[] args) {
