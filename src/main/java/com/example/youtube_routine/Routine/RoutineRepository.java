c97bc77e5d8366bc16d16fa468010a725e2bd921
lbk00 <lbk6661@gmail.com>
2025-03-22 14:54:47 +0900

    루틴 조회 메서드 추가

--- src/main/java/com/example/youtube_routine/Routine/RoutineRepository.java
+++ src/main/java/com/example/youtube_routine/Routine/RoutineRepository.java
@@ -19,5 +19,7 @@ public interface RoutineRepository extends JpaRepository<Routine, Long> {
     List<Routine> findByRoutineTime(String routineTime);
     // 사용자별 루틴 조회
     List<Routine> findByUser(User user);
-
+    // days 컬럼에 현재 요일이 포함된 루틴 조회
+    @Query("SELECT r FROM Routine r WHERE r.routineTime = :routineTime AND r.days LIKE %:day%")
+    List<Routine> findByRoutineTimeAndDay(String routineTime, String day);
 }
