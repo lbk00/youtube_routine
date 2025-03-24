46a8a62a54e758d29818d0b4cf1435c68270fddc
lbk00 <lbk6661@gmail.com>
2025-03-24 16:40:17 +0900

    deleteByFcmToken 추가

--- src/main/java/com/example/youtube_routine/User/UserRepository.java
+++ src/main/java/com/example/youtube_routine/User/UserRepository.java
@@ -6,5 +6,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByFcmToken(String fcmToken);
-
+    void deleteByFcmToken(String fcmToken);
 }
